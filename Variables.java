6715e4c99062a428610402f21c5c64f22b36123b
ARATIANANTSAHANE <162159080+ARATIANANTSAHANE@users.noreply.github.com>
2025-04-04 14:43:33 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+    //Local Variables : Declare,Inilitize inside method
+    //Global Variable : declare Inside class
+    //Static Variables : Declare inside class
+    //variables :Store values : container
+int x=10;
+    public static void main(String[] args) {
+        // Method Body
+        //Local
+        Variables obj = new Variables();
+        System.out.println(obj.x);
+    }
+}
