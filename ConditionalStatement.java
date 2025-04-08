64ad15639787ddbed0b75e61442eb63d6ea7bb7e
ARATIANANTSAHANE <162159080+ARATIANANTSAHANE@users.noreply.github.com>
2025-04-08 17:53:28 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,42 @@
+public class ConditionalStatement {
+    
+    public static void main(String[] args) {
+        int day=5;{
+            switch (day) {
+                
+                   case 1:
+                     System.out.println("Monday");
+                    break;
+
+                    case 2:
+                    System.out.println("Tuesday");
+                    break;
+
+                    case 3:
+                    System.out.println("Wednsday");
+                    break;
+                
+                    case 4:
+                    System.out.println("Thrusday");
+                    break;
+
+                    case 5:
+                    System.out.println("friday");
+                    break;
+
+                    case 6:
+                    System.out.println("saturday");
+                    break;
+                
+                    case 7:
+                    System.out.println("sunday");
+                    break;
+                     
+                    default:
+                System.out.println("correct");
+                
+            }
+        }
+        
+    }
+}
